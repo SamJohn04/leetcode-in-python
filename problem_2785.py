d22aacbf3c80715918686a0643781cbfdcfcdb62
SamJohn04 <samuel.john.codes@gmail.com>
2025-09-11 14:19:34 +0530

    accept: 2785

---
+++ problem_2785.py
@@ -0,0 +1,58 @@
+# Given a 0-indexed string s, permute s to get a new string t such that:
+# 
+# All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+# The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+# Return the resulting string.
+# 
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: s = "lEetcOde"
+# Output: "lEOtcede"
+# Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+# Example 2:
+# 
+# Input: s = "lYmpH"
+# Output: "lYmpH"
+# Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+#  
+# 
+# Constraints:
+# 
+# 1 <= s.length <= 105
+# s consists only of letters of the English alphabet in uppercase and lowercase.
+
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        all_vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+        vowels_in_s = [x for x in s if x in all_vowels]
+        vowels_in_s = sorted(vowels_in_s)
+        
+        index = 0
+        str_as_list = []
+        for x in s:
+            if x in all_vowels:
+                str_as_list.append(vowels_in_s[index])
+                index += 1
+            else:
+                str_as_list.append(x)
+        
+        return "".join(str_as_list)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    a = "lEetcOde"
+    print("--- CASE 1 ---")
+    print(a, sep=", ")
+    print(sol.sortVowels(a))
+
+    a = "lYmpH"
+    print("--- CASE 2 ---")
+    print(a, sep=", ")
+    print(sol.sortVowels(a))
+
