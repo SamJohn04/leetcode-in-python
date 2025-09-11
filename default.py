5c5b5809f95c7a214045b3b7023ed33bd7cb56bb
SamJohn04 <samuel.john.codes@gmail.com>
2025-09-11 07:03:51 +0530

    add default.py

---
+++ default.py
@@ -0,0 +1,13 @@
+class Solution:
+    pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print("--- CASE 1 ---")
+    print(sep=", ")
+
+    print("--- CASE 2 ---")
+    print(sep=", ")
+
